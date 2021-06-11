2908b088f44f13e2bef6efde9ed3e65d4b61080d
Dominik Krížo <dominik.krizo@doxxbet.sk>
2021-06-11 17:07:28 +0200

    create liquidity value calculator

---
+++ contracts/LiquidityValueCalculator.sol
@@ -0,0 +1,37 @@
+pragma solidity >=0.6.6;
+
+import '@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol';
+import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
+
+contract LiquidityValueCalculator {
+
+    using SafeMath for uint;
+
+    address public factory;
+    address public wethAddress;
+    address public usdtAddress;
+
+    constructor(address factory_, address weth, address usdt) public {
+        factory = factory_;
+        wethAddress = weth;
+        usdtAddress = usdt;
+    }
+
+    function pairInfo(address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB, uint totalSupply) {
+        IUniswapV2Pair pair = IUniswapV2Pair(UniswapV2Library.pairFor(factory, tokenA, tokenB));
+        totalSupply = pair.totalSupply();
+        (uint reserves0, uint reserves1,) = pair.getReserves();
+        (reserveA, reserveB) = tokenA == pair.token0() ? (reserves0, reserves1) : (reserves1, reserves0);
+    }
+
+    function tokenPriceInEther(address tokenAddress) public view returns (uint) {
+        (uint tokenReserve, uint wEthReserve, uint supply) = pairInfo(tokenAddress, wethAddress);
+        return wEthReserve / tokenReserve;
+    }
+
+    function ethPrice() public view returns (uint) {
+        (uint usdtReserve, uint wEthReserve, uint supply) = pairInfo(usdtAddress, wethAddress);
+        return usdtReserve / wEthReserve;
+    }
+
+}
--- migrations/2_token_migration.js
+++ migrations/2_token_migration.js
@@ -1,7 +1,39 @@
 const Ribe = artifacts.require("Ribe");
 
-module.exports = async function (deployer) {
-  // create shiba treat token
+const uniswapJson = require("@uniswap/v2-core/build/UniswapV2Factory.json");
+const contract = require("@truffle/contract");
+const UniSwap = contract(uniswapJson);
+UniSwap.setProvider(this.web3._provider);
+
+module.exports = async function (deployer, network, accounts) {
+  // deploy uniswap
+  const UniswapV2Factory = await deployer.deploy(UniSwap, accounts[0], {from : accounts[0]});
+  
+  // test prices: ETHER PRICE, TOKEN PRICE
+  
+  // deploy ETHER
+  // TODO
+
+  // deploy USDT
+  // TODO
+  
+  // deploy TOKEN
+  // TODO
+  
+  // create pairs
+  
+  // create ETHER - USDT pair
+  // TODO
+  
+  // create ETHER - TOKEN pair
+  // TODO
+
+  // deploy liquidity value calculator
+  // TODO
+
+  // check prices
+
+  // deploy riba dec port contract
   await deployer.deploy(Ribe);
   let tokenInstance = await Ribe.deployed();
 };
